e942055e59a7a53668b14310ac7ea3d9511ff387
Andrea Venuta <venutawebdesign@gmail.com>
2025-12-18 13:06:42 +0100

    Day 10 part 2 bruteforce not working

--- src/bin/day10.rs
+++ src/bin/day10.rs
@@ -9,16 +9,16 @@ use adventofcode2025::get_input;
 struct Input {
     lights: u32,
     buttons: Vec<u32>,
+    joltages: Vec<u32>,
 }
 
 fn parse(line: &str) -> Input {
-    let mut it = line.split_whitespace();
-    let lights_s = it
-        .next()
-        .unwrap()
+    let (lights, buttons) = line.split_once(' ').unwrap();
+    let (buttons, joltages) = buttons.rsplit_once(' ').unwrap();
+
+    let lights = lights
         .trim_start_matches('[')
-        .trim_end_matches(']');
-    let lights = lights_s
+        .trim_end_matches(']')
         .chars()
         .filter_map(|c| match c {
             '#' => Some(true),
@@ -28,26 +28,31 @@ fn parse(line: &str) -> Input {
         .enumerate()
         .fold(0u32, |o, (idx, i)| o | if i { 1 << idx } else { 0 });
 
-    println!("{lights_s} -> {lights:012b}");
-
-    let buttons = it
-        .take_while(|s| s.starts_with('('))
+    let buttons = buttons
+        .split_whitespace()
         .map(|btn| {
-            let v = btn
-                .trim_start_matches('(')
+            btn.trim_start_matches('(')
                 .trim_end_matches(')')
                 .split(',')
                 .fold(0u32, |o, i| {
                     let pow = i.parse::<usize>().unwrap();
                     o | 1 << pow
-                });
-
-            println!("{btn} -> {v:010b}");
-            v
+                })
         })
         .collect::<Vec<_>>();
 
-    Input { lights, buttons }
+    let joltages = joltages
+        .trim_start_matches('{')
+        .trim_end_matches('}')
+        .split(',')
+        .map(|i| i.parse::<u32>().unwrap())
+        .collect::<Vec<_>>();
+
+    Input {
+        lights,
+        buttons,
+        joltages,
+    }
 }
 
 impl Input {
@@ -72,27 +77,88 @@ impl Input {
 
         None
     }
+
+    fn bfs2(&self) -> Option<u64> {
+        self.power_set()
+            .flat_map(|ops| self.bfs2_inner(&ops))
+            .take(100)
+            .min()
+    }
+
+    fn bfs2_inner(&self, ops: &[Vec<usize>]) -> Option<u64> {
+        println!("Opping {ops:?}");
+        let mut seen = HashSet::new();
+        let mut q = VecDeque::new();
+        q.push_back(vec![0u32; ops.len()]);
+
+        while let Some(pushes) = q.pop_front() {
+            let mut joltages = vec![0u32; self.joltages.len()];
+
+            pushes.iter().zip(ops.iter()).for_each(|(&push, ops)| {
+                for &op in ops {
+                    joltages[op] += push;
+                }
+            });
+
+            if joltages == self.joltages {
+                let tot_pushes = pushes.iter().sum::<u32>() as u64;
+                if !seen.contains(&pushes) {
+                    seen.insert(pushes.clone());
+                    println!("{pushes:?} x {ops:?} = {joltages:?} ~ {:?}", self.joltages);
+                    println!("{tot_pushes} tot");
+                    return Some(tot_pushes);
+                }
+            }
+
+            for i in 0..ops.len() {
+                let mut pushes = pushes.clone();
+                pushes[i] += 1;
+
+                if joltages
+                    .iter()
+                    .zip(self.joltages.iter())
+                    .all(|(a, b)| a <= b)
+                {
+                    q.push_back(pushes);
+                }
+            }
+        }
+
+        None
+    }
+
+    fn power_set<'a>(self: &'a Input) -> impl Iterator<Item = Vec<Vec<usize>>> + 'a {
+        (0..(1usize << self.buttons.len() as u32)).map(|bits| {
+            self.buttons
+                .iter()
+                .enumerate()
+                .filter_map(move |(idx, &b)| {
+                    if (1 << idx) & bits != 0 {
+                        Some(b)
+                    } else {
+                        None
+                    }
+                })
+                .map(|bits| {
+                    (0..16)
+                        .filter(|i| ((bits >> i) & 1) == 1)
+                        .collect::<Vec<usize>>()
+                })
+                .collect::<Vec<_>>()
+        })
+    }
 }
 
 fn part1(input: &str) -> u64 {
     let lines = input.lines().map(parse).collect::<Vec<_>>();
 
-    // for line in lines {
-    //     println!("{:08b}", line.lights);
-    //     for op in &line.buttons {
-    //         print!("{op:08b} ");
-    //     }
-    //     println!("\n");
-    //
-    //     println!("{:?}: {:?}", line, line.bfs());
-    // }
-
-    println!("{}", lines.len());
     lines.iter().filter_map(Input::bfs).sum::<u64>()
 }
 
-fn part2(_input: &str) -> u64 {
-    0
+fn part2(input: &str) -> u64 {
+    let lines = input.lines().map(parse).collect::<Vec<_>>();
+
+    lines.iter().filter_map(Input::bfs2).sum::<u64>()
 }
 
 fn main() {
